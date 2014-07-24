afb833b9015b64d049b47e7eb5392c617312eb34
Jesse McGinnis <jessieelsom@hotmail.com>
2014-07-24 16:25:45 -0400

    Not really working yet, but getting the structure there.

--- lib/roleable.rb
+++ lib/roleable.rb
@@ -1,5 +1,11 @@
+require 'active_record'
+require 'active_support/inflector'
+
 require 'roleable/version'
+require 'roleable/acts_as_roleable'
 
 module Roleable
-  # Your code goes here...
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.extend Roleable::ActsAsRoleable
+  end
 end
