3f87d5fa2ddf73a661353cfaf6902e235b311c76
Jesse McGinnis <jessieelsom@hotmail.com>
2014-07-24 15:48:51 -0400

    Split out test_helper

---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'minitest/pride'     # awesome colorful output
+require 'minitest/reporters' # fancier output format
+require 'pry'
+
+require 'roleable'
